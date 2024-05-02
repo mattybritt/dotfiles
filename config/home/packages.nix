ae44373f5b1c712b3e60f83a5f29d25bd5e92ed9
Matt Britt <mattybritt@gmail.com>
2024-05-02 17:37:26 +0200

    Install tmux

--- config/home/packages.nix
+++ config/home/packages.nix
@@ -9,7 +9,7 @@ in {
     pkgs."${browser}" discord libvirt swww grim slurp gnome.file-roller
     swaynotificationcenter rofi-wayland imv transmission-gtk mpv
     gimp obs-studio rustup audacity pavucontrol tree protonup-qt
-    font-awesome spotify swayidle neovide vscode
+    font-awesome spotify swayidle neovide vscode tmux
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
     # Import Scripts
     (import ./../scripts/emopicker9000.nix { inherit pkgs; })
