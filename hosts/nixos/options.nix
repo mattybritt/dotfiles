74e5ee3198cc7728688f3e01b67a3f40de29b993
Matt Britt <mattybritt@gmail.com>
2024-05-09 11:47:43 +0200

    Edit screen resolution

--- hosts/nixos/options.nix
+++ hosts/nixos/options.nix
@@ -27,8 +27,8 @@ in {
   # Hyprland Settings
   borderAnim = true; # Enable / Disable Hyprland Border Animation
   extraMonitorSettings = "
-    monitor=DP-1,2560x1440@60,0x0,1
-    monitor=HDMI-A-1,2560x1440@60,2560x0,1
+    monitor=DP-1,3840x2160@60,0x0,1
+    monitor=HDMI-A-1,3840x2160@60,3840x0,1
   ";
 
   # Waybar Settings
