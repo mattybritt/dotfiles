e534d658229fa6599b9a4544afbca34623efa0b1
Matt Britt <mattybritt@gmail.com>
2024-05-16 08:28:22 +0200

    Add dap ui

--- config/home/nixvim/dap.nix
+++ config/home/nixvim/dap.nix
@@ -2,9 +2,20 @@
   programs.nixvim = {
     plugins.dap = {
       enable = true;
+      extensions= {
+        dap-ui.enable = true;
+        dap-virtual-text.enable = true;
+        dap-go.enable = true;
+        dap-python.enable = true;
+      };
     };
     keymaps = [
-       {
+        {
+          mode = "n";
+          key = "<leader>dt";
+          action = "<cmd>DapUiToggle<CR>";
+        }
+        {
           mode = "n";
           key = "<leader>db";
           action = "<cmd>DapToggleBreakpoint<CR>";
