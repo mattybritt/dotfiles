819c543b8a615e13f99e4029d211c8d914cba852
Matt Britt <mattb@property24.com>
2024-05-27 16:29:53 +0200

    Add ssh config

--- config/home/files.nix
+++ config/home/files.nix
@@ -5,6 +5,7 @@
   home.file.".emoji".source = ./files/emoji;
   home.file.".base16-themes".source = ./files/base16-themes;
   home.file.".config/starship.toml".source = ./files/starship.toml;
+  home.file.".ssh/config".source = ./files/config;
   home.file.".scripts" = {
     source = ./files/scripts;
     recursive = true;
