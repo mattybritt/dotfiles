cad331356af964c305f8204dd55183bad859126a
Matt Britt <mattybritt@gmail.com>
2024-07-03 15:12:21 +0200

    Alter monitors

--- hosts/nixos/options.nix
+++ hosts/nixos/options.nix
@@ -18,8 +18,8 @@ in {
   # Hyprland Settings
   borderAnim = true; # Enable / Disable Hyprland Border Animation
   extraMonitorSettings = "
-    monitor=DP-1,3840x2160@60,0x0,1
-    monitor=HDMI-A-1,3840x2160@60,3840x0,1
+    monitor=HDMI-A-1,3840x2160@60,0x0,1
+    monitor=DP-1,3840x2160@60,3840x0,1
   ";
 
   # Waybar Settings
