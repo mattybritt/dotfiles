5db80b2c10f5607261acb43eee5d8c0883dad2bd
Matt Britt <mattybritt@gmail.com>
2024-08-05 15:48:31 +0200

    Remove obs and gimp

--- config/home/packages.nix
+++ config/home/packages.nix
@@ -8,9 +8,9 @@ in {
   home.packages = with pkgs; [
     pkgs."${browser}" slack whatsapp-for-linux libvirt swww grim slurp gnome.file-roller
     swaynotificationcenter rofi-wayland imv transmission-gtk mpv
-    gimp obs-studio audacity pavucontrol tree protonup-qt
+    audacity pavucontrol tree protonup-qt
     font-awesome spotify swayidle vscode azuredatastudio
-    bitwarden speedtest-cli tmuxifier zathura obsidian gammastep burpsuite firefox
+    bitwarden speedtest-cli tmuxifier zathura gammastep burpsuite firefox
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
     # Import Scripts
     (import ./../scripts/emopicker9000.nix { inherit pkgs; })
