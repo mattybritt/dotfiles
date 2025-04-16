024a4f857b794b1585d73da4f01b17e96e280703
Matt Britt <mattybritt@gmail.com>
2025-04-16 18:28:07 +0200

    Remove cheat sheet

--- config/home/hyprland.nix
+++ config/home/hyprland.nix
@@ -143,7 +143,6 @@ in
                  exec-once = waybar
                  exec-once = swaync
                  exec-once = wallsetter
-                 exec-once = cheatsheet-launcher
                  exec-once = nm-applet --indicator
                  exec-once = swayidle -w timeout 720 'swaylock -f' timeout 800 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' before-sleep 'swaylock -f -c 000000'
                  dwindle {
