a8b0ed520055143b3576a30d132ddd36f176a64f
Matt Britt <mattybritt@gmail.com>
2025-05-02 14:27:33 +0200

    Add bash configs

--- config/home/files.nix
+++ config/home/files.nix
@@ -1,5 +1,7 @@
 {...}: {
   home.file.".emoji".source = ./files/emoji;
+  home.file.".bashrc".source = ./files/.bashrc;
+  home.file.".bash_aliases".source = ./files/.bash_aliases;
   home.file.".base16-themes".source = ./files/base16-themes;
   home.file.".config/starship.toml".source = ./files/starship.toml;
   home.file.".config/gammastep/config.ini".source = ./files/gammastep/config.ini;
